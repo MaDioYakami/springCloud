ac0e28d216be7664913888b8ee6219793c8c171a
madio <madioyakami@gmail.com>
2019-04-29 14:11:43 +0800

    添加ribbon + restTemplate的消费者通过负载均衡调用rest

---
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/service/AdminService.java
@@ -0,0 +1,19 @@
+package com.funtl.hello.spring.cloud.web.admin.ribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author madio
+ */
+@Service
+public class AdminService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String sayHi(String message) {
+        return restTemplate.getForObject("http://HELLO-SPRING-CLOUD-SERVICE-ADMIN/hi?message=" + message, String.class);
+    }
+}
